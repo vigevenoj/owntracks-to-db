4db0026bdd94dae9b7a169929d731ab63c2a44b0
Jacob Vigeveno <vigevenoj@gmail.com>
2017-05-07 09:56:08 -0700

    python3 flavor, should work, needs some testing

--- owntracks_to_db.py
+++ owntracks_to_db.py
@@ -4,10 +4,12 @@ Store owntracks location updates in a database
 """
 from datetime import datetime, timezone
 import json
+import logging
 import paho.mqtt.client as mqtt
 import psycopg2
 import ssl
 import sys
+import time
 import yaml
 
 
@@ -27,14 +29,26 @@ class Dumper():
                                               user=dbuser, password=dbpass,
                                               dbname=dbname)
 
+                # Handle mqtt messages
+                def handle_message(client, userdata, message):
+                    j = json.loads(str(message.payload, encoding="ascii"))
+                    if(message.topic.startswith("owntracks")):
+                        # If message format is 'owntracks/user/device'
+                        # then we should try to parse out userid/deviceid
+                        # and handle this message as a location update
+                        if(j['_type'] == 'location'):
+                            userid = message.topic.split('/')[1]
+                            device = message.topic.split('/')[2]
+                            self.handle_location_update(userid, device, j)
+
                 self._client = mqtt.Client(client_id="")
                 try:
                     self._client.tls_set(ca_certs=configs['mqtt']['ca'],
                                          cert_reqs=ssl.CERT_REQUIRED,
                                          tls_version=ssl.PROTOCOL_TLSv1)
                 except IOError as e:
-                    print("Something went wrong while setting up mqtt. {0}"
-                          .format(e))
+                    logging.error("Something went wrong setting up mqtt. {0}"
+                                  .format(e))
                 self._client.username_pw_set(configs['mqtt']['username'],
                                              configs['mqtt']['password'])
                 self._client.will_set("/lwt/o2db",
@@ -42,46 +56,50 @@ class Dumper():
                 mqtt_host = configs['mqtt']['host']
                 mqtt_port = configs['mqtt']['port']
                 self._client.connect(mqtt_host, mqtt_port)
-                # Don't subscribe until after we have the database set up
-                # self._client.subscribe([("owntracks/#', 0)])
+                self._client.on_message = handle_message
         except IOError as e:
-            print("Unable to load configuration. {0}".format(e))
+            logging.error("Unable to load configuration. {0}".format(e))
             sys.exit(1)
 
     def handle_location_update(self, user, device, rawdata):
-        acc = rawdata['acc']  # accuracy in meters
-        alt = rawdata['alt']  # altitude above sea level
-        batt = rawdata['batt']  # battery percentage
-        cog = rawdata['cog']  # course over ground
+        acc = rawdata.get('acc')  # accuracy in meters
+        alt = rawdata.get('alt')  # altitude above sea level
+        batt = rawdata.get('batt')  # battery percentage
+        cog = rawdata.get('cog')  # course over ground
         lat = rawdata['lat']  # latitude
         lon = rawdata['lon']  # longitude
-        rad = rawdata['rad']  # radius around region
-        t = rawdata['t']  # p, c, b, r, u, t; see booklet
-        tid = rawdata['tid']  # tracker ID
+        rad = rawdata.get('rad')  # radius around region
+        t = rawdata.get('t')  # p, c, b, r, u, t; see booklet
+        tid = rawdata.get('tid')  # tracker ID
         tst = datetime.fromtimestamp(rawdata['tst'], timezone.utc)  # Timestamp
-        vac = rawdata['vac']  # vertical accuracy
-        vel = rawdata['vel']  # velocity, kmh
-        pressure = rawdata['p']  # barometric pressure in kPa (float)
-        connection_status = rawdata['conn']  # connection status: w, o, m
+        vac = rawdata.get('vac')  # vertical accuracy
+        vel = rawdata.get('vel')  # velocity, kmh
+        pressure = rawdata.get('p')  # barometric pressure in kPa (float)
+        connection_status = rawdata.get('conn')  # connection status: w, o, m
 
         # execute prepared statement
         cur = self._conn.cursor()
         cur.execute(
-            """insert into locationupdates (acc, alt, batt, cog, lat, lon, radius, t, tid, tst, vac, vel, p, conn, rawdata)
-            values (%(acc)s, %(alt)s, %(batt)s, %(cog)s, %(lat)s, %(lon)s, %(rad)s, %(t)s, %(tid)s, %(tst)s, %(vac)s, %(vel)s, %(p)s, %(conn)s, %(rawdata)s);""",
-            {'acc': acc, 'alt': alt, 'batt': batt, 'cog': cog, 'lat': lat, 'lon': lon, 'rad': rad, 't': t, 'tid': tid, 'tst': tst, 'vac': vac, 'vel': vel, 'p': pressure, 'conn': connection_status, 'rawdata': rawdata})
-
-    def handle_message(self):
-        # Handle a sinle owntrakcs message
-        # topic will be 'owntracks/user/device'
-        # the message will be a json hash
-        pass
+            """insert into locationupdates (acc, alt, batt, cog, lat, lon,
+            radius, t, tid, tst, vac, vel, p, conn, rawdata)
+            values (%(acc)s, %(alt)s, %(batt)s, %(cog)s, %(lat)s, %(lon)s,
+            %(rad)s, %(t)s, %(tid)s, %(tst)s, %(vac)s, %(vel)s, %(p)s,
+            %(conn)s, %(rawdata)s);""",
+            {'acc': acc, 'alt': alt, 'batt': batt, 'cog': cog, 'lat': lat,
+             'lon': lon, 'rad': rad, 't': t, 'tid': tid, 'tst': tst,
+             'vac': vac, 'vel': vel, 'p': pressure, 'conn': connection_status,
+             'rawdata': json.dumps(rawdata)})
 
     def run(self):
         self._client.subscribe([("owntracks/#", 0)])
-        pass
+        logging.warn("subscribed to 'owntracks/#'")
+        self._client.loop_forever()
+        try:
+            while True:
+                time.sleep(2)
+        except (KeyboardInterrupt, SystemExit):
+            self._client.disconnect()
 
 if __name__ == '__main__':
     dumper = Dumper()
-    # dumper._client.loop_forever()
     dumper.run()
